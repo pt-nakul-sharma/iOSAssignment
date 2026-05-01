027dfd9df174ba38c70198ea1a745970d9299cc9
pt-nakul-sharma <nakulsharma.1296@gmail.com>
2026-05-01 14:39:51 +0530

    Initial Commit

---
+++ iOSAssignment/iOSAssignmentApp.swift
@@ -0,0 +1,32 @@
+//
+//  iOSAssignmentApp.swift
+//  iOSAssignment
+//
+//  Created by Nakul Sharma on 01/05/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct iOSAssignmentApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ iOSAssignmentTests/iOSAssignmentTests.swift
@@ -0,0 +1,19 @@
+//
+//  iOSAssignmentTests.swift
+//  iOSAssignmentTests
+//
+//  Created by Nakul Sharma on 01/05/26.
+//
+
+import Testing
+@testable import iOSAssignment
+
+struct iOSAssignmentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
